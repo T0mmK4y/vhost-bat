d2679b14ea9e4fb5a2c03090f600cf9f63fcf2dc
T0mmK4y <t0mmk4y@gmail.com>
2013-06-06 13:59:00 +0300

    Finished:  - folder check and creation  - httpd-vhosts.conf editin  - hosts editing  - index.php creation

--- createdomain.bat
+++ createdomain.bat
@@ -7,15 +7,16 @@ SET BASEURL=D:\web\vhosts\
 ::ask for a sitename
 set /p sitename="Enter Sitename: " %=%
 
-echo Before IF
 IF EXIST %BASEURL%%sitename% echo The specified site folder already exists! Choose another. 
 IF EXIST %BASEURL%%sitename% goto sitenameprompt 
 
 IF NOT EXIST %BASEURL%%sitename% echo Creating folder 
 IF NOT EXIST %BASEURL%%sitename% MD %BASEURL%%sitename%
+IF NOT EXIST %BASEURL%%sitename%/index.php echo ^<?php phpinfo(); ?^> >%BASEURL%%sitename%/index.php
 
 echo Adding virtualhost to httpd.conf
 (
+echo.
 echo ###%sitename%### 
 echo	^<VirtualHost *:80^> 
 echo		ServerAdmin admin@%sitename% 
@@ -28,10 +29,11 @@ echo			Order allow,deny
 echo			Allow from all 
 echo		^</Directory^> 
 echo	^</VirtualHost^> 
-) >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
+echo.
+) >>D:\web\xampp\apache\conf\extra\httpd-vhosts.conf
 
 echo Updating hosts
-echo  127.0.0.1	%sitename%.dev  >>C:\Windows\System32\Drivers\etc
+echo  127.0.0.1	%sitename%.dev  >>C:\Windows\System32\Drivers\etc\hosts
 
 echo Restarting apache
 httpd -k restart
