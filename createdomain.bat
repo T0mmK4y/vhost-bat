99f2319824dc9b1f181b95762378dd1b2145ef74
T0mmK4y <t0mmk4y@gmail.com>
2013-06-06 13:17:07 +0300

    Finished

--- createdomain.bat
+++ createdomain.bat
@@ -15,22 +15,24 @@ IF NOT EXIST %BASEURL%%sitename% echo Creating folder
 IF NOT EXIST %BASEURL%%sitename% MD %BASEURL%%sitename%
 
 echo Adding virtualhost to httpd.conf
-pause
+(
+echo ###%sitename%### 
+echo	^<VirtualHost *:80^> 
+echo		ServerAdmin admin@%sitename% 
+echo		DocumentRoot "%BASEURL%%sitename%" 
+echo		ServerName %sitename%.dev 
+echo    	ServerAlias %sitename%.dev 
+echo		^<Directory "%BASEURL%%sitename%"^> 
+echo			Options Indexes FollowSymLinks Includes ExecCGI 
+echo			Order allow,deny 
+echo			Allow from all 
+echo		^</Directory^> 
+echo	^</VirtualHost^> 
+) >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
 
-echo ###%sitename%### >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo	^<VirtualHost *:80^> >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo		ServerAdmin admin@%sitename% >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo		DocumentRoot "%BASEURL%%sitename%" >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo		ServerName %sitename%.dev >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo    	ServerAlias %sitename%.dev >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo		^<Directory "%BASEURL%%sitename%"^> >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo			Options Indexes FollowSymLinks Includes ExecCGI >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo			Order allow,deny >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo			Allow from all >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo		^</Directory^> >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
-echo	^</VirtualHost^> >>D:\web\xampp\apache\conf\extra\httpd-vhostsTest.conf
+echo Updating hosts
+echo  127.0.0.1	%sitename%.dev  >>C:\Windows\System32\Drivers\etc
 
-pause
 echo Restarting apache
-::httpd -k restart
+httpd -k restart
 
